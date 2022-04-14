0e10502a24740179bea6ab3ee7d4ebac9b45d31c
Jamie Hill <jamie.hill@metronetinc.com>
2022-04-14 06:33:44 -0500

    Updating signin URI

--- src/lib/cognitoUtils.js
+++ src/lib/cognitoUtils.js
@@ -33,7 +33,8 @@ const createCognitoUserPool = () => new CognitoUserPool({
 
 // Get the URI of the hosted sign in screen
 const getCognitoSignInUri = () => {
-  const signinUri = `${appConfig.userPoolBaseUri}/login?response_type=code&client_id=${appConfig.clientId}&redirect_uri=${appConfig.callbackUri}`
+  //const signinUri = `${appConfig.userPoolBaseUri}/login?response_type=code&client_id=${appConfig.clientId}&redirect_uri=${appConfig.callbackUri}`
+  const signinUri = `${appConfig.userPoolBaseUri}/login?response_type=token&client_id=${appConfig.clientId}&redirect_uri=${appConfig.callbackUri}&scope=email+openid`
   return signinUri
 }
 
