cfd580aaedf5358fd93508dbe842bd1fce9b822b
Jamie Hill <jamie.hill@metronetinc.com>
2022-04-15 03:47:35 -0500

    testing

--- src/routes/Callback.js
+++ src/routes/Callback.js
@@ -26,6 +26,7 @@ class Callback extends Component {
 
   render () {
     // If there's no auth code in the URL or we're now logged into, redirect to the root page
+    console.log(this.props);
     if ((!this.props.location.hash && !this.props.location.search) || this.props.session.isLoggedIn) {
       return <Redirect to="/" />
     }
